6433d7db72ed5dc3e6c509765321a4dae053873b
Islam Bekbuzarov <n157318@gmail.com>
2024-12-01 22:20:38 +0300

    updated simple example

--- libmemtrack/examples/simple/src/main.rs
+++ libmemtrack/examples/simple/src/main.rs
@@ -1,12 +1,10 @@
-use std::sync::Arc;
-
 fn main() {
     Box::new(10i32);
     Box::new(20i64);
     Box::new(30i128);
-    vec![1u8, 2, 3];
 
-    Arc::new(2u8);
+    let _: Vec<u8> = Vec::with_capacity(3);
+
     String::from("hello");
     String::from("h");
 }
