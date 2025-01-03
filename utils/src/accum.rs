bfbbf879eeed2142cceb59d75463cba91c7d083a
Islam Bekbuzarov <n157318@gmail.com>
2025-01-03 16:59:42 +0300

    added traces and frames

--- utils/src/accum.rs
+++ utils/src/accum.rs
@@ -12,14 +12,44 @@ pub enum Error {
 }
 
 #[derive(Debug)]
+struct Trace {
+    ip_idx: u64,
+    parent_idx: u64,
+}
+
+#[derive(Debug)]
+struct InstructionPointer {
+    ip: u64,
+    module_idx: usize,
+    frame: Frame,
+    inlined: Vec<Frame>,
+}
+
+#[derive(Debug)]
+pub enum Frame {
+    Single {
+        function_idx: usize,
+    },
+    Multiple {
+        function_idx: usize,
+        file_idx: usize,
+        line_number: u16,
+    },
+}
+
+#[derive(Debug)]
 struct AccumulatedData {
     strings: Vec<String>,
+    traces: Vec<Trace>,
+    instruction_pointers: Vec<InstructionPointer>,
 }
 
 impl AccumulatedData {
     pub fn new() -> Self {
         Self {
             strings: Vec::with_capacity(4096),
+            traces: Vec::with_capacity(65536),
+            instruction_pointers: Vec::with_capacity(16384),
         }
     }
 }
@@ -55,9 +85,62 @@ fn parse_line(line: &str, data: &mut AccumulatedData) -> Result<(), Error> {
             data.strings
                 .push(split.next().ok_or(Error::InvalidFormat)?.to_string());
         }
+        "t" => {
+            let ip_idx = u64::from_str_radix(split.next().ok_or(Error::InvalidFormat)?, 16)
+                .map_err(|_| Error::InvalidFormat)?;
+            let parent_idx = u64::from_str_radix(split.next().ok_or(Error::InvalidFormat)?, 16)
+                .map_err(|_| Error::InvalidFormat)?;
+
+            data.traces.push(Trace { ip_idx, parent_idx })
+        }
+        "i" => {
+            let ip = u64::from_str_radix(split.next().ok_or(Error::InvalidFormat)?, 16)
+                .map_err(|_| Error::InvalidFormat)?;
+            let module_idx = usize::from_str_radix(split.next().ok_or(Error::InvalidFormat)?, 16)
+                .map_err(|_| Error::InvalidFormat)?;
+
+            let frame = parse_frame(&mut split)?.ok_or(Error::InvalidFormat)?;
+            let mut inlined = Vec::new();
+
+            while let Some(frame) = parse_frame(&mut split)? {
+                inlined.push(frame);
+            }
+
+            data.instruction_pointers.push(InstructionPointer {
+                ip,
+                module_idx,
+                frame,
+                inlined,
+            })
+        }
         _ => {}
     }
     Ok(())
+}
+
+fn parse_frame<'a>(mut iter: impl Iterator<Item = &'a str>) -> Result<Option<Frame>, Error> {
+    let Some(first) = iter.next() else {
+        return Ok(None);
+    };
+
+    let function_idx = usize::from_str_radix(first, 16).map_err(|_| Error::InvalidFormat)?;
+
+    let Some(file_val) = iter.next() else {
+        return Ok(Some(Frame::Single { function_idx }));
+    };
+
+    let file_idx = usize::from_str_radix(file_val, 16).map_err(|_| Error::InvalidFormat)?;
+    let line_number = iter
+        .next()
+        .unwrap_or_default()
+        .parse()
+        .map_err(|_| Error::InvalidFormat)?;
+
+    Ok(Some(Frame::Multiple {
+        function_idx,
+        file_idx,
+        line_number,
+    }))
 }
 
 #[cfg(test)]
